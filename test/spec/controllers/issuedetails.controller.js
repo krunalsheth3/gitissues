ff70bdb04a8288cc57331236dc9a50b77f193c5f
Krunal Sheth <krunal.sheth@verizon.com>
2016-08-14 22:54:15 -0400

    Fixed typos

--- test/spec/controllers/issuedetails.controller.js
+++ test/spec/controllers/issuedetails.controller.js
@@ -16,19 +16,19 @@ describe('Controller: IssuedetailsCtrl', function () {
       return link;
     },
     status: 200,
-    statusText: "OK"
+    statusText: 'OK'
   };
 
   var mockCommentsData = [
     {},
     {}
-  ]
+  ];
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, defaultConst, sharedSvc, issuedetailsSvc, $stateParams) {
+  beforeEach(inject(function ($controller, defaultConst, sharedSvc, issuedetailsSvc) {
 
-    mockDefaultConst = defaultConst
-    mockSharedSvc = sharedSvc,
+    mockDefaultConst = defaultConst;
+    mockSharedSvc = sharedSvc;
     mockIssueDetailsSvc = issuedetailsSvc;
 
     IssuedetailsCtrl = $controller('IssuedetailsCtrl', {
@@ -39,49 +39,50 @@ describe('Controller: IssuedetailsCtrl', function () {
 
 
   it('totalItems should be a numerical Values', function () {
-    console.log("test for totalItems should be a valid number: ",IssuedetailsCtrl.totalItems );
+    console.log('test for totalItems should be a valid number: ',IssuedetailsCtrl.totalItems );
     expect(isNaN(IssuedetailsCtrl.totalItems)).toBeFalsy();
   });
 
   it('should have itemsPerPage be a numerical Values', function () {
-    console.log("test for itemsPerPage should be a valid number: ",IssuedetailsCtrl.itemsPerPage );
+    console.log('test for itemsPerPage should be a valid number: ',IssuedetailsCtrl.itemsPerPage );
     expect(isNaN(IssuedetailsCtrl.itemsPerPage)).toBeFalsy();
   });
 
   it('should have maxPaginationSize be a numerical Values & should be 25', function () {
-    console.log("test for maxPaginationSize should be a valid number & it should be 25: ",IssuedetailsCtrl.maxPaginationSize);
+    console.log('test for maxPaginationSize should be a valid number & it should be 25: ',IssuedetailsCtrl.maxPaginationSize);
     expect(isNaN(IssuedetailsCtrl.maxPaginationSize)).toBeFalsy();
     expect(IssuedetailsCtrl.maxPaginationSize).toBe(5);
   });
 
   it('should have currentPage be a numerical value', function () {
-    console.log("test for currentPage should be a valid number : ",IssuedetailsCtrl.currentPage);
+    console.log('test for currentPage should be a valid number : ',IssuedetailsCtrl.currentPage);
     expect(isNaN(IssuedetailsCtrl.currentPage)).toBeFalsy();
 
   });
 
   it('should have returned an object with color set into it', function() {
-    var color = "FF0000";
+    var color = 'FF0000';
     var actualSetColor = IssuedetailsCtrl.set_color(color);
 
-    color = "#" + color;
+    color = '#' + color;
     var expectedSetColor = {
       'background-color': color,
       'color': '#FFFFFF',
       'height': '16px',
       'line-height': 1
-    }
-    console.log("test for expected color when calling set_color ");
+    };
+
+    console.log('test for expected color when calling set_color ');
     expect(actualSetColor).toEqual(expectedSetColor);
     expect(actualSetColor).toBeTruthy();
   });
 
   it('should have response object to be valid when comments is more than 0', function () {
-    console.log("test for issuedetails comments if greater or less than 0");
+    console.log('test for issuedetails comments if greater or less than 0');
     if(IssuedetailsCtrl.issueDetails[0].comments > 0) {
       expect(IssuedetailsCtrl.commentsData).toBeDefined();
     } else {
-      expect(IssuedetailsCtrl.commentsData).toEqual("No comments available");
+      expect(IssuedetailsCtrl.commentsData).toEqual('No comments available');
     }
-  })
+  });
 });
--- test/spec/controllers/main.controller.js
+++ test/spec/controllers/main.controller.js
@@ -14,11 +14,11 @@ describe('Controller: MainCtrl', function () {
       return link;
     },
     status: 200,
-    statusText: "OK"
+    statusText: 'OK'
   };
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, mainSvc, defaultConst, sharedSvc, $state){
+  beforeEach(inject(function ($controller, mainSvc, defaultConst, sharedSvc){
     mockMainSvc  = mainSvc;
     mockDefaultConst = defaultConst;
     mockSharedSvc = sharedSvc;
@@ -30,46 +30,46 @@ describe('Controller: MainCtrl', function () {
 
 
   it('should have defaultOrder set to created', function () {
-    console.log("test for defaultSortOrder: ", MainCtrl.defaultSortOrder);
+    console.log('test for defaultSortOrder: ', MainCtrl.defaultSortOrder);
     expect(MainCtrl.defaultSortOrder).toBe('created');
   });
   it('should have state set to all', function () {
-    console.log("test for default value of state: ", MainCtrl.state);
+    console.log('test for default value of state: ', MainCtrl.state);
     expect(MainCtrl.state).toBe('all');
   });
   it('should have direction set to desc', function () {
-    console.log("test for default value of direction: ", MainCtrl.direction);
+    console.log('test for default value of direction: ', MainCtrl.direction);
     expect(MainCtrl.direction).toBe('desc');
   });
   it('should have dateSortText set to Newest', function () {
-    console.log("test for default value of dateSortText: ", MainCtrl.dateSortText);
+    console.log('test for default value of dateSortText: ', MainCtrl.dateSortText);
     expect(MainCtrl.dateSortText).toBe('Newest');
   });
   it('totalItems should be a numerical Values', function () {
-    console.log("test for totalItems should be a valid number: ",MainCtrl.totalItems );
+    console.log('test for totalItems should be a valid number: ',MainCtrl.totalItems );
     expect(isNaN(MainCtrl.totalItems)).toBeFalsy();
   });
 
   it('should have itemsPerPage be a numerical Values', function () {
-    console.log("test for itemsPerPage should be a valid number: ",MainCtrl.itemsPerPage );
+    console.log('test for itemsPerPage should be a valid number: ',MainCtrl.itemsPerPage );
     expect(isNaN(MainCtrl.itemsPerPage)).toBeFalsy();
   });
 
   it('should have maxPaginationSize be a numerical Values & should be 25', function () {
-    console.log("test for maxPaginationSize should be a valid number & it should be 25: ",MainCtrl.maxPaginationSize);
+    console.log('test for maxPaginationSize should be a valid number & it should be 25: ',MainCtrl.maxPaginationSize);
     expect(isNaN(MainCtrl.maxPaginationSize)).toBeFalsy();
     expect(MainCtrl.maxPaginationSize).toBe(5);
   });
 
   it('should have currentPage be a numerical value', function () {
-    console.log("test for currentPage should be a valid number : ",MainCtrl.currentPage);
+    console.log('test for currentPage should be a valid number : ',MainCtrl.currentPage);
     expect(isNaN(MainCtrl.currentPage)).toBeFalsy();
 
   });
 
 
   it('should have typesOfStates set to valid array of objects', function () {
-    console.log("test for default value of typesOfStates: ", MainCtrl.typesOfStates);
+    console.log('test for default value of typesOfStates: ', MainCtrl.typesOfStates);
     expect(MainCtrl.typesOfStates).toEqual([
       {name: 'open'},
       {name: 'closed'},
@@ -78,7 +78,7 @@ describe('Controller: MainCtrl', function () {
   });
 
   it('should have typesOfSorting set to valid array of objects', function () {
-    console.log("test for default value of typesOfSorting: ", MainCtrl.typesOfSorting);
+    console.log('test for default value of typesOfSorting: ', MainCtrl.typesOfSorting);
     expect(MainCtrl.typesOfSorting).toEqual([
       {name: 'Recently Created', value: 'created'},
       {name: 'Recently updated', value: 'updated'},
@@ -87,21 +87,20 @@ describe('Controller: MainCtrl', function () {
   });
 
   it('should have returned an object with color set into it', function() {
-    var color = "FF0000";
+    var color = 'FF0000';
     var actualSetColor = MainCtrl.set_color(color);
 
-    color = "#" + color;
+    color = '#' + color;
     var expectedSetColor = {
       'background-color': color,
       'color': '#FFFFFF',
       'height': '16px',
       'line-height': 1
-    }
-    console.log("test for expected color when calling set_color ");
+    };
+
+    console.log('test for expected color when calling set_color ');
     expect(actualSetColor).toEqual(expectedSetColor);
     expect(actualSetColor).toBeTruthy();
-
-
   });
 
 });
